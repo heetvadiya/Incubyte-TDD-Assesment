139b5779b8bb6d961f7736f78e858444ce3292af
heetvadiya <heetvadiya@gmail.com>
2024-07-30 02:40:12 +0530

    [Add Step-4] Passed test : any given delimiter

--- src/main/java/org/heetvadiya/StringCalculator.java
+++ src/main/java/org/heetvadiya/StringCalculator.java
@@ -10,11 +10,27 @@ public class StringCalculator {
         if(input.isEmpty()){
             return 0;
         }
-        String[] numbers = input.split(",|\\n");
         int sum = 0;
-        for(String number : numbers) {
-            sum += parseInt(number);
+        if(input.startsWith("//")){
+            int del_start_ind = input.indexOf("//");
+            int del_end_ind = input.indexOf("\n");
+
+            String delimiter = input.substring(del_start_ind+2, del_end_ind);
+
+            String onlynumbers = input.substring(del_end_ind+1, input.length());
+
+            String[] numbers = onlynumbers.split(delimiter);
+            for(String number : numbers) {
+                sum += parseInt(number);
+            }
         }
+        else{
+            String[] numbers = input.split(",|\\n");
+            for(String number : numbers) {
+                sum += parseInt(number);
+            }
+        }
+
         return sum;
     }
 }
