90be36baf2e79d8b5f9951ab5ccf39a0dcd6f552
heetvadiya <heetvadiya@gmail.com>
2024-07-30 02:20:24 +0530

    [Add Step-1] Added failing test single number str should return that number

--- src/test/java/org/heetvadiya/StringCalculatorTest.java
+++ src/test/java/org/heetvadiya/StringCalculatorTest.java
@@ -10,4 +10,12 @@ public class StringCalculatorTest {
     public void empty_string_should_return_0() {
         assertEquals(0,stringCalculator.add(""));
     }
+
+    @Test
+    public void single_number_string_should_return_that_number() {
+        assertEquals(1,stringCalculator.add("1"));
+        assertEquals(20,stringCalculator.add("20"));
+    }
+
+
 }
