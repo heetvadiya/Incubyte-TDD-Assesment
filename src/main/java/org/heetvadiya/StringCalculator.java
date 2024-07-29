b8dedfce00aabcea90531ece43e34f96dccf1d5b
heetvadiya <heetvadiya@gmail.com>
2024-07-30 02:22:06 +0530

    [Add Step-1] Passed test : single number str should return that number

--- src/main/java/org/heetvadiya/StringCalculator.java
+++ src/main/java/org/heetvadiya/StringCalculator.java
@@ -2,12 +2,17 @@ package org.heetvadiya;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Integer.parseInt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculator {
     public int add(String input) {
         if(input.isEmpty()){
             return 0;
+        }
+        String[] numbers = input.split(",");
+        if (numbers.length == 1) {
+            return parseInt(numbers[0]);
         }
         return 0;
     }
